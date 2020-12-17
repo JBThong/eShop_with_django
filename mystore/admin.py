3f844be53f4979c70dd1086d6c33bb45a7d8f666
Thong Bui <qktc1422@gmail.com>
2020-12-17 23:58:34 +0700

    edit cate model and render at adminsite

--- mystore/admin.py
+++ mystore/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Product, Category
 
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description', 'image', 'cate_id']
+
+class AdminCate(admin.ModelAdmin):
+    list_display = ['name']
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCate)
--- mystore/models/category.py
+++ mystore/models/category.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    name = models.TextField()
+    name = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.name
