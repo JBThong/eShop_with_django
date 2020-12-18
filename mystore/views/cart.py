c165f7e869795b514e7bb521902262b1e45d71db
Thong Bui <qktc1422@gmail.com>
2020-12-18 23:56:34 +0700

    check no product in cart

--- mystore/views/cart.py
+++ mystore/views/cart.py
@@ -7,9 +7,13 @@ from mystore.models import Product
 class Cart(View):
     def get(self, request):
         carts = request.session.get('cart')
+        products = None
+        is_has_product = False
         if carts:
             ids = list(carts.keys())
             products = Product.get_products_by_id(ids)
-            # print(products.name)
-        return render(request, 'cart.html', {'products': products})
+        print(type(products))
+        if products:
+            is_has_product = True
+        return render(request, 'cart.html', {'products': products, 'is_has_product': is_has_product})
 
