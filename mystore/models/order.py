c2e2957a8ef47ed497d251a677c6611408f32368
Thong Bui <qktc1422@gmail.com>
2020-12-19 10:12:47 +0700

    update model order

--- mystore/models/order.py
+++ mystore/models/order.py
@@ -1,17 +1,24 @@
+
 from django.db import models
 from .product import Product
 from .user import User
 import datetime
 
-
+# order_status = types.Enum('new', 'confirmed', 'paid',
+#                           'shipping', 'shipped', 'fulfilled', 'cancel',
+#                           name='order_status')
 class Order(models.Model):
-    product = models.ForeignKey(Product,
-                                on_delete=models.CASCADE)
-    customer = models.ForeignKey(User,
-                                 on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     price = models.IntegerField()
     address = models.CharField(max_length=50, default='', blank=True)
     phone = models.CharField(max_length=50, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
-    status = models.BooleanField(default=False)
+    status = models.TextField(blank=False, default='new')
+
+class OrderDetail(models.Model):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_id')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_orderdetail_id')
+    quantity = models.IntegerField(default=1)
+    price = models.BigIntegerField()
+    date = models.DateField(default=datetime.datetime.today)
